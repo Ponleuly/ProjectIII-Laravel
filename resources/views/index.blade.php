afce6b352b4113b8e7f311806ac2d0339ec58c40
Ponleuly <lyponleu116@gmail.com>
2022-10-25 15:34:14 +0700

    frontend: created page of nav menu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+ <!-- /*
+* Bootstrap 5
+* Template Name: Furni
+* Template Author: Untree.co
+* Template URI: https://untree.co/
+* License: https://creativecommons.org/licenses/by/3.0/
+*/ -->
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="{{url('frontend/css/bootstrap.min.css')}}">
+		<link rel="stylesheet" href="{{url('frontend/css/tiny-slider.css')}}">
+		<link rel="stylesheet" href="{{url('frontend/css/style.css')}}">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
+	</head>
+
+	<body>
+
+		<!-- Start Header/Navigation -->
+		@include('nav')
+		<!-- End Header/Navigation -->
+ 	
+		<!-- main display/body  -->
+		@yield('content')
+		<!-- End main display -->
+
+		<!-- Start Footer Section -->
+		@include('footer')
+		<!-- End Footer Section -->	
+
+		<script src="{{url('frontend/js/bootstrap.bundle.min.js')}}"></script>
+		<script src="{{url('frontend/js/tiny-slider.js')}}"></script>
+		<script src="{{url('frontend/js/custom.js')}}"></script>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', function(){
+   return view('index'); 
+});
+Route::get('/home', function(){
+   return view('home'); 
+});
+Route::get('/shop', function(){
+   return view('shop'); 
+});
+Route::get('/about', function(){
+   return view('about'); 
+});
+Route::get('/services', function(){
+   return view('services'); 
+});
+Route::get('/blog', function(){
+   return view('blog'); 
+});
+Route::get('/contact', function(){
+   return view('contact'); 
+});
+Route::get('/cart', function(){
+   return view('cart'); 
+});
+
