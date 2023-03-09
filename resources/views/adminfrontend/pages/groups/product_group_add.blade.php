69293aa0335a6f8d8630328e0168cadef6eccc02
Ponleuly <lyponleu116@gmail.com>
2023-03-09 20:04:08 +0700

    update table title on group

--- resources/views/adminfrontend/pages/groups/product_group_add.blade.php
+++ resources/views/adminfrontend/pages/groups/product_group_add.blade.php
@@ -12,18 +12,18 @@
                     </div>
 		            @endif
 
-                    <h4 class="mb-2 text-black">Add product group</h4>
+                    <h4 class="mb-2 text-black">Add Groups</h4>
                     <div class="p-3 p-lg-4 border bg-white">
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group mb-2">
                                     <div class="col-md-12">
-                                        <label for="group_name"><p class="text-label">Product group name</p></label>
+                                        <label for="group_name"><p class="text-label">Group Title</p></label>
                                         <input type="text" class="form-control rounded-0 fw-500 mb-2 text-capitalize" id="group_name" name="group_name" placeholder="group name...">
 
                                         <div class="d-flex mt-4">
-                                            <a class="btn btn-outline-danger rounded-0 mt-3" href="{{url('/admin/product-group-list')}}" role="button">Back to list</a>
-                                            <button class="btn btn-primary rounded-0 ms-auto mt-3" group="submit">Add product group</button>
+                                            <a class="btn btn-outline-danger rounded-0 mt-3" href="{{url('/admin/product-group-list')}}" role="button">Back to List</a>
+                                            <button class="btn btn-primary rounded-0 ms-auto mt-3" group="submit">Add Group</button>
                                         </div>
                                     </div>
                                 </div>
--- resources/views/adminfrontend/pages/groups/product_group_list.blade.php
+++ resources/views/adminfrontend/pages/groups/product_group_list.blade.php
@@ -15,11 +15,11 @@
                         </div>
 		            @endif
 
-                    <h4 class="mb-2 text-black">Product groups</h4>
+                    <h4 class="mb-2 text-black">Groups List</h4>
                     <div class="p-3 p-lg-4 border bg-white">
                         <div class="row">
                             <div class="col-md-12 d-flex">
-                                <a class="btn btn-outline-primary rounded-0" href="{{url('/admin/product-group-add')}}" role="button">Add Product Group</a>
+                                <a class="btn btn-outline-primary rounded-0" href="{{url('/admin/product-group-add')}}" role="button">Add Groups</a>
                                 <div class="input-group w-25 ms-auto">
                                     <input group="search" class="form-control rounded-0" placeholder="Search here..." aria-label="Recipient's username" aria-describedby="search">
                                     <button class="btn btn-outline-primary rounded-0" group="button" id="search">Search</button>
@@ -51,8 +51,26 @@
                                         </td>
                                         <td>{{$row->created_at->diffForHumans()}}</td>
                                         <td>
-                                            <a class="btn btn-primary py-1 px-2 btn-sm" href="{{url('/admin/product-group-edit/'.$row->id)}}" role="button">Edit</a>
-                                            <a class="btn btn-danger py-1 px-2 btn-sm" href="{{url('/admin/product-group-delete/'.$row->id)}}" role="button">Delete</a>
+                                            <a
+                                                class="text-light py-1 pb-0 px-2 rounded-0 edit-btn"
+                                                href="{{url('/admin/product-group-edit/'.$row->id)}}"
+                                                role="button"
+                                                data-bs-toggle="tooltip"
+                                                data-bs-placement="top"
+                                                title="Edit Product"
+                                                >
+                                                <span class="material-icons-round" style="font-size: 16px">edit</span>
+                                            </a>
+                                            <a
+                                                class="text-light py-1 pb-0 px-2 rounded-0 delete-btn"
+                                                href="{{url('/admin/product-group-delete/'.$row->id)}}"
+                                                role="button"
+                                                data-bs-toggle="tooltip"
+                                                data-bs-placement="top"
+                                                title="Delete Product"
+                                                >
+                                                <span class="material-icons-round" style="font-size: 16px">delete</span>
+                                            </a>
                                         </td>
                                     </tr>
                                 @endforeach
